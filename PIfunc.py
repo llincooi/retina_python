2b2c47af1cce702a7597d325ece8aa4ba6ef3559
llincooi <33573023+llincooi@users.noreply.github.com>
2020-07-28 17:10:07 +0800

    0728p

--- PIfunc.py
+++ PIfunc.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import numpy as np
+import os
+os.chdir('C:\\Users\\hydro_leo\\Documents\\GitHub\\BROJA_2PID')
+from BROJA_2PID import pid, BROJA_2PID_Exception
 ## PIfunc that calculate Unique, Synergy and Redundancy of three unsymmetric varibles
+def Histo3D2Dict(P):
+    PDF = dict()
+    for i in range(np.size(P, 0)):
+        for j in range(np.size(P, 1)):
+            for k in range(np.size(P, 2)):
+                PDF[(k,i,j)] = float(P[i,j,k])
+    return PDF
+
 def PIfunc(r, x, v, dt, window):
     negshift=window[0] # second
     posshift=window[1] # second
     shiftlen=(posshift-negshift)/dt+1
     timeshift=np.linspace(negshift,posshift,int(shiftlen))
     bitshift=np.linspace(negshift/dt,posshift/dt,int(shiftlen),dtype = 'int16')
+    Information = dict()
+    Information[('BROJA_2PID','SI')]=np.zeros(len(bitshift))
+    Information[('BROJA_2PID','UIx')]=np.zeros(len(bitshift))
+    Information[('BROJA_2PID','UIv')]=np.zeros(len(bitshift))
+    Information[('BROJA_2PID','CI')]=np.zeros(len(bitshift))
+    Information[('Beer','Red')]=np.zeros(len(bitshift))
+    Information[('Beer','UIx')]=np.zeros(len(bitshift))
+    Information[('Beer','UIv')]=np.zeros(len(bitshift))
+    Information[('Beer','Syn')]=np.zeros(len(bitshift))
+    
+    Information[('test','SI')]=np.zeros(len(bitshift))
+    Information[('test','UIx')]=np.zeros(len(bitshift))
+    Information[('test','UIv')]=np.zeros(len(bitshift))
+    Information[('test','CI')]=np.zeros(len(bitshift))
     Redun =np.zeros(len(bitshift))
     MIxr=np.zeros(len(bitshift))
     MIvr=np.zeros(len(bitshift))
     MIxvR=np.zeros(len(bitshift))
+    parms = dict()
+    parms['max_iters'] = 20
     # shifted data
     # shift>0 => y shifted to positive side
     for i in range(len(bitshift)):
@@ -50,12 +77,33 @@ def PIfunc(r, x, v, dt, window):
         pvr=(np.sum(N,axis=0)/np.sum(N))[np.newaxis, :, :]
         pxvr=(N/np.sum(N))
         
-        MIxr[i]=np.nansum(pxr*np.log2(pxr/px/pr))/dt
-        MIvr[i]=np.nansum(pvr*np.log2(pvr/pv/pr))/dt
-        MIxvR[i]=np.nansum(pxvr*np.log2(pxvr/pxv/pr))/dt
+        Information[('test','UIx')][i] = np.nansum(pxvr*np.log2(pxvr*px/pxv/pxr))/dt
+        Information[('test','UIv')][i] = np.nansum(pxvr*np.log2(pxvr*pv/pxv/pvr))/dt
+                
+        PDF=Histo3D2Dict(pxvr)
+        BROJA_2PID = pid(PDF, cone_solver="ECOS", output=0, **parms)
+        Information[('BROJA_2PID','SI')][i]=BROJA_2PID['SI']/dt
+        Information[('BROJA_2PID','UIx')][i]=BROJA_2PID['UIY']/dt
+        Information[('BROJA_2PID','UIv')][i]=BROJA_2PID['UIZ']/dt
+        Information[('BROJA_2PID','CI')][i]=BROJA_2PID['CI']/dt
         
+
+        MIxr=np.nansum(pxr*np.log2(pxr/px/pr))/dt
+        MIvr=np.nansum(pvr*np.log2(pvr/pv/pr))/dt
+        MIxvR=np.nansum(pxvr*np.log2(pxvr/pxv/pr))/dt
         PI_xR = np.nansum(pxr*np.log2(pxr/px/pr), axis = (0,1))
         PI_vR = np.nansum(pvr*np.log2(pvr/pv/pr), axis = (0,1))
-        Redun[i] = sum(np.minimum(PI_xR, PI_vR))/dt
-    return timeshift, MIxr, MIvr, MIxvR, Redun
+        R = sum(np.minimum(PI_xR, PI_vR))/dt
+        Information[('Beer','Red')][i] = R
+        Information[('Beer','UIx')][i] = MIxr - R
+        Information[('Beer','UIv')][i] = MIvr - R
+        Information[('Beer','Syn')][i] = MIxvR - MIxr - MIvr + R
+
+    return timeshift, Information
+
+
+# In[ ]:
+
+
+
 
